08970bc8de7a3fd7f140ef7f745fee21954e6ad7
anilyavas <anil.yavas99@gmail.com>
2025-03-02 17:56:22 +0300

    Auth file structure and Auth install

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,21 +1,20 @@
 import { Slot } from 'expo-router';
-//import { ClerkProvider, ClerkLoaded } from '@clerk/clerk-expo';
-//import { tokenCache } from './../../cache';
+import { ClerkProvider, ClerkLoaded } from '@clerk/clerk-expo';
+import { tokenCache } from '../cache';
+const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!;
 
-//const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!;
-
-//if (!publishableKey) {
-//  throw new Error(
-//    'Missing Publishable Key. Please set EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY in your .env'
-//  );
-//}
+if (!publishableKey) {
+  throw new Error(
+    'Missing Publishable Key. Please set EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY in your .env'
+  );
+}
 
 export default function RootLayout() {
   return (
-    //<ClerkProvider tokenCache={tokenCache} publishableKey={publishableKey}>
-    //<ClerkLoaded>
-    <Slot />
-    //</ClerkLoaded>
-    //</ClerkProvider>
+    <ClerkProvider tokenCache={tokenCache} publishableKey={publishableKey}>
+      <ClerkLoaded>
+        <Slot />
+      </ClerkLoaded>
+    </ClerkProvider>
   );
 }
